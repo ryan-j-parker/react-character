083866d86df51d8d8fb722a5ed687dafe988defa
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-09-16 13:38:01 -0700

    add functions and elements to change slogan and add it to an array

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Editor.css';
 
-export default function Editor({ head, setHead, tops, setTops, bottoms, setBottoms, headCounter, setHeadCounter, topsCounter, setTopsCounter, bottomsCounter, setBottomsCounter }) {
+export default function Editor({ head, setHead, tops, setTops, bottoms, setBottoms, headCounter, setHeadCounter, topsCounter, setTopsCounter, bottomsCounter, setBottomsCounter, catchphrase, setCatchphrase, catchphraseArray, setCatchphraseArray }) {
   return (
     <div className="editor">
       <div className="change">Select a head
@@ -28,6 +28,11 @@ export default function Editor({ head, setHead, tops, setTops, bottoms, setBotto
       <div className="counter-display">
         <p>You have changed your head {headCounter} times, your top {topsCounter} times, and your bottoms {bottomsCounter} times</p>
       </div>
+      <div className="catchphrase">
+        <textarea type="text" name="catchphrase" value={catchphrase} onChange={(e) => setCatchphrase(e.target.value)} />
+        <button onClick={() => { return setCatchphraseArray([...catchphraseArray, catchphrase]), setCatchphrase(''); }}>Add a catchphrase</button>
+      </div>
+      <div className="catchphrase-display"><ul>{[catchphraseArray]}</ul></div>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -15,11 +15,13 @@ export default function Main() {
   const [headCounter, setHeadCounter] = useState(0);
   const [topsCounter, setTopsCounter] = useState(0);
   const [bottomsCounter, setBottomsCounter] = useState(0);
+  const [catchphrase, setCatchphrase] = useState('');
+  const [catchphraseArray, setCatchphraseArray] = useState([]);
 
   return (
     <main style={{ backgroundImage: `url(${background})` }} >
       <div className="main">
-        <Editor head={head} setHead={setHead} tops={tops} setTops={setTops} bottoms={bottoms} setBottoms={setBottoms} headCounter={headCounter} setHeadCounter={setHeadCounter} topsCounter={topsCounter} setTopsCounter={setTopsCounter} bottomsCounter={bottomsCounter} setBottomsCounter={setBottomsCounter} />
+        <Editor head={head} setHead={setHead} tops={tops} setTops={setTops} bottoms={bottoms} setBottoms={setBottoms} headCounter={headCounter} setHeadCounter={setHeadCounter} topsCounter={topsCounter} setTopsCounter={setTopsCounter} bottomsCounter={bottomsCounter} setBottomsCounter={setBottomsCounter} catchphrase={catchphrase} setCatchphrase={setCatchphrase} catchphraseArray={catchphraseArray} setCatchphraseArray={setCatchphraseArray} />
         <Preview head={head} tops={tops} bottoms={bottoms} />
       </div>
     </main>
